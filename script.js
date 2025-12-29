df04875e8f825aec51f7c0759a62cee058a3c447
Debasish ghosh <119937911+Debasishghosh24@users.noreply.github.com>
2025-12-29 21:21:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// var roleSwitch=new Typed(".role",{
+//     strings:["Frontend Developer","Web Designer","Software Engineer","Coder"],
+//     typeSpeed:80,
+//     backSpeed:80,
+//     backDelay:100,
+//     loop:true
+// });
+// alert("hllodvfwjeh");
+var tupeSwitch=new Typed(".role",{
+    strings:["frontend Developer","web Designer", "software Engineer", "coder"],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay:1000,
+    loop:true,
+    showCursor: false,
+});
